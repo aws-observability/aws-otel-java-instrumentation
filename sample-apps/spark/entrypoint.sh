c209ad6a75c56871d0a39454db10a526d6eff72b
bhautikpip <bhautip@amazon.com>
2020-10-28 13:30:13 -0700

    Added spark sample app

---
+++ sample-apps/spark/entrypoint.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+/app/bin/TraceEmitter
---
+++ sample-apps/spark/src/main/java/com/amazon/sampleapp/App.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.sampleapp;
+
+import io.opentelemetry.trace.TracingContextUtils;
+
+import java.io.UncheckedIOException;
+import java.util.Random;
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.OkHttpClient;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.io.IOException;
+
+import static spark.Spark.*;
+
+public class App {
+    static final String REQUEST_START_TIME = "requestStartTime";
+
+    private static MetricEmitter buildMetricEmitter(){
+        return new MetricEmitter();
+    }
+
+    public static void main(String[] args) {
+        MetricEmitter metricEmitter = buildMetricEmitter();
+        final Call.Factory httpClient = new OkHttpClient();
+        final S3Client s3 = S3Client.builder().build();
+        String port;
+        String host;
+        String listenAddress = System.getenv("LISTEN_ADDRESS");
+
+        if (listenAddress == null) {
+            host = "0.0.0.0";
+            port = "4567";
+        } else {
+            String[] splitAddress = listenAddress.split(":");
+            host = splitAddress[0];
+            port = splitAddress[1];
+        }
+
+        // set sampleapp app port number and ip address
+        port(Integer.parseInt(port));
+        ipAddress(host);
+
+        get("/", (req, res) -> {
+            return "healthcheck";
+        });
+
+        /**
+         * trace http request
+         */
+        get("/outgoing-http-call", (req, res) -> {
+            try (Response response =
+                         httpClient.newCall(new Request.Builder().url("https://aws.amazon.com").build()).execute()) {
+            } catch (IOException e) {
+                throw new UncheckedIOException("Could not fetch endpoint", e);
+            }
+
+            return getXrayTraceId();
+        });
+
+        /**
+         * trace aws sdk request
+         */
+        get("/aws-sdk-call", (req, res) -> {
+            s3.listBuckets();
+
+            return getXrayTraceId();
+        });
+
+
+        /**
+         * record a start time for each request
+         */
+        before((req, res) -> {
+            req.attribute(REQUEST_START_TIME, System.currentTimeMillis());
+        });
+
+        after((req, res) -> {
+            // for below paths we don't emit metric data
+            if (req.pathInfo().equals("/outgoing-http-call")) {
+                String statusCode = String.valueOf(res.status());
+                // calculate return time
+                Long requestStartTime = req.attribute(REQUEST_START_TIME);
+                metricEmitter.emitReturnTimeMetric(
+                        System.currentTimeMillis()- requestStartTime,
+                        req.pathInfo(),
+                        statusCode
+                );
+
+                // emit http request load size
+                metricEmitter.emitBytesSentMetric(
+                        req.contentLength() + mimicPayloadSize(),
+                        req.pathInfo(),
+                        statusCode);
+            }
+        });
+
+        exception(Exception.class, (exception, request, response) -> {
+            // Handle the exception here
+            exception.printStackTrace();
+        });
+    }
+
+    // get x-ray trace id
+    private static String getXrayTraceId() {
+        String traceId = TracingContextUtils.getCurrentSpan().getContext().getTraceIdAsHexString();
+        String xrayTraceId = "1-" + traceId.substring(0, 8) + "-" + traceId.substring(8);
+
+        return String.format("{\"traceId\": \"%s\"}", xrayTraceId);
+    }
+
+    private static int mimicPayloadSize() {
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(1000);
+    }
+}
