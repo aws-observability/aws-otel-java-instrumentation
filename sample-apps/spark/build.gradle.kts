1cc9d9bc2202034a03e2ae64738b6af7733ff026
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-04 13:12:47 +0900

    Don't set version or group in sample apps.

--- sample-apps/spark/build.gradle.kts
+++ sample-apps/spark/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
   id("com.google.cloud.tools.jib") version "2.5.0"
 }
 
-group = "com.amazon.sampleapp"
-version = "1.0"
-
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
