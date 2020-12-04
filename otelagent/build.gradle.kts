3b26222851c68da79eb7d3f455ef99947846af09
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-04 13:14:50 +0900

    Target java 8 for agent.

--- otelagent/build.gradle.kts
+++ otelagent/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_7
-  targetCompatibility = JavaVersion.VERSION_1_7
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 base {
