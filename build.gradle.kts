c0754c24b3174aad7218c452efd7609e66c027ab
Anuraag Agrawal <anuraaga@gmail.com>
2021-04-08 08:12:44 +0900

    Add an AWS composite propagator that can be used in AWS-service compatible projects. (#50)

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,7 @@ allprojects {
 
       testImplementation("org.assertj:assertj-core")
       testImplementation("org.junit.jupiter:junit-jupiter-api")
+      testImplementation("org.junit.jupiter:junit-jupiter-params")
       testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,6 +38,7 @@ dependencyResolutionManagement {
 }
 
 include(":awsagentprovider")
+include(":awspropagator")
 include(":dependencyManagement")
 include(":instrumentation:logback-1.0")
 include(":instrumentation:log4j-2.13.2")
