ef36916cc4b512788c62f45b9afea99ad5aba0b8
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-23 09:56:47 +0900

    Generate javadoc / sources jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,9 @@ allprojects {
     java {
       sourceCompatibility = JavaVersion.VERSION_1_8
       targetCompatibility = JavaVersion.VERSION_1_8
+
+      withJavadocJar()
+      withSourcesJar()
     }
 
     dependencies {
