61449d09bd34e33ce459e2d18b7254cbad743907
github-action-benchmark <github@users.noreply.github.com>
2021-09-29 20:11:10 +0000

    add Soak Test Results - sample-app-spark-auto (custombenchmark) benchmark result for a49aa48ce7a7eaece23dff0f773cc9495980746e

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632946260013,
+  "lastUpdate": 1632946270881,
   "repoUrl": "https://github.com/aws-observability/aws-otel-java-instrumentation",
   "entries": {
     "Soak Test Results - sample-app-spark-awssdkv1-auto": [
@@ -65,6 +65,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average Virtual Memory Used",
             "value": 3488.2797683189656,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ],
+    "Soak Test Results - sample-app-spark-auto": [
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "a49aa48ce7a7eaece23dff0f773cc9495980746e",
+          "message": "Fix gh-pages link for auto Soak Test links (#92)",
+          "timestamp": "2021-09-29T04:51:56Z",
+          "url": "https://github.com/aws-observability/aws-otel-java-instrumentation/commit/a49aa48ce7a7eaece23dff0f773cc9495980746e"
+        },
+        "date": 1632946270439,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 23.646465517241378,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 3460.6529947916665,
             "unit": "Megabytes"
           }
         ]
