151d3d729fda0e4785c2002075c49ca990015f59
Anuraag Agrawal <anuraaga@gmail.com>
2022-04-21 00:59:54 +0900

    Update Gradle and build dependencies (#166)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,11 +15,11 @@
 
 pluginManagement {
   plugins {
-    id("com.diffplug.spotless") version "6.3.0"
+    id("com.diffplug.spotless") version "6.4.2"
     id("com.github.ben-manes.versions") version "0.42.0"
     id("com.github.jk1.dependency-license-report") version "2.1"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("com.google.cloud.tools.jib") version "3.2.0"
+    id("com.google.cloud.tools.jib") version "3.2.1"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("nebula.release") version "16.0.0"
     id("org.springframework.boot") version "2.6.6"
