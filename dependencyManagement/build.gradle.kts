1f2926c437d1fc1f8c3eb7ca6a78179ea1a28954
Raphael Silva <rapphil@gmail.com>
2022-12-30 00:24:49 +0000

    Update dependency tracking script
    Signed-off-by: Raphael Silva <rapphil@gmail.com>

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -113,7 +113,7 @@ rootProject.allprojects {
 
 fun isNonStable(version: String): Boolean {
   val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val regex = "^[0-9,.v-]+(-r|-alpha)?$".toRegex()
   val isGuava = version.endsWith("-jre")
   val isStable = stableKeyword || regex.matches(version) || isGuava
   return isStable.not()
