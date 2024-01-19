760991459c0a53b683943ceb2ad6015955bdbca3
Vasi Vasireddy <pvasir@amazon.com>
2024-01-18 16:51:17 -0800

    Remove Patches and Update dependencies

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -27,31 +27,31 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val TEST_SNAPSHOTS = rootProject.findProperty("testUpstreamSnapshots") == "true"
 
 // This is the version of the upstream instrumentation BOM
-val otelVersion = "1.32.0-adot1"
-val otelSnapshotVersion = "1.33.0"
+val otelVersion = "2.0.0"
+val otelSnapshotVersion = "2.1.0"
 val otelAlphaVersion = if (!TEST_SNAPSHOTS) "$otelVersion-alpha" else "$otelSnapshotVersion-alpha-SNAPSHOT"
 val otelJavaAgentVersion = if (!TEST_SNAPSHOTS) otelVersion else "$otelSnapshotVersion-SNAPSHOT"
 // All versions below are only used in testing and do not affect the released artifact.
 
 val DEPENDENCY_BOMS = listOf(
-  "com.amazonaws:aws-java-sdk-bom:1.12.599",
-  "com.fasterxml.jackson:jackson-bom:2.16.0",
+  "com.amazonaws:aws-java-sdk-bom:1.12.640",
+  "com.fasterxml.jackson:jackson-bom:2.16.1",
   "com.google.guava:guava-bom:33.0.0-jre",
-  "com.google.protobuf:protobuf-bom:3.25.1",
+  "com.google.protobuf:protobuf-bom:3.25.2",
   "com.linecorp.armeria:armeria-bom:1.26.4",
-  "io.grpc:grpc-bom:1.59.1",
+  "io.grpc:grpc-bom:1.61.0",
   "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:$otelAlphaVersion",
-  "org.apache.logging.log4j:log4j-bom:2.21.1",
+  "org.apache.logging.log4j:log4j-bom:2.22.1",
   "org.junit:junit-bom:5.10.1",
   "org.springframework.boot:spring-boot-dependencies:2.7.17",
   "org.testcontainers:testcontainers-bom:1.19.3",
-  "software.amazon.awssdk:bom:2.21.33",
+  "software.amazon.awssdk:bom:2.23.6",
 )
 
 val DEPENDENCY_SETS = listOf(
   DependencySet(
     "org.assertj",
-    "3.24.2",
+    "3.25.1",
     listOf("assertj-core"),
   ),
   DependencySet(
@@ -70,14 +70,14 @@ val DEPENDENCY_SETS = listOf(
 )
 
 val DEPENDENCIES = listOf(
-  "commons-logging:commons-logging:1.2",
+  "commons-logging:commons-logging:1.3.0",
   "com.sparkjava:spark-core:2.9.4",
   "com.squareup.okhttp3:okhttp:4.12.0",
   "io.opentelemetry.contrib:opentelemetry-aws-xray:1.32.0",
   "io.opentelemetry.contrib:opentelemetry-aws-resources:1.32.0-alpha",
   "io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha",
   "io.opentelemetry.javaagent:opentelemetry-javaagent:$otelJavaAgentVersion",
-  "net.bytebuddy:byte-buddy:1.14.10",
+  "net.bytebuddy:byte-buddy:1.14.11",
 )
 
 javaPlatform {
