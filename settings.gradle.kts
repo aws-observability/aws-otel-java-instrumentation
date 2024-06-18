fa76acc4ddf8b3d28ee4d27259fabe530b60699d
Thomas Pierce <thp@amazon.com>
2024-06-18 16:28:19 -0700

    Upgrade jib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
     id("com.github.ben-manes.versions") version "0.50.0"
     id("com.github.jk1.dependency-license-report") version "2.5"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.google.cloud.tools.jib") version "3.4.0"
+    id("com.google.cloud.tools.jib") version "3.4.3"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     id("nebula.release") version "18.0.6"
     id("org.springframework.boot") version "2.7.17"
