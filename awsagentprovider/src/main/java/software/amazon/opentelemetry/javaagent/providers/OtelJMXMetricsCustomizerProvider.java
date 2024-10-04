b07f361d37a332597ae33cb097a3c6454a24a139
Mitali Salvi <mitsalvi@amazon.com>
2024-10-04 19:43:33 -0400

    removing delta temporality and adding condition for checking both bool and endpoint string

--- awsagentprovider/src/main/java/software/amazon/opentelemetry/javaagent/providers/OtelJMXMetricsCustomizerProvider.java
+++ awsagentprovider/src/main/java/software/amazon/opentelemetry/javaagent/providers/OtelJMXMetricsCustomizerProvider.java
@@ -40,105 +40,105 @@ import java.util.logging.Logger;
  * environment variable OTEL_JMX_ENABLED_CONFIG. This flag is disabled by default.
  */
 public class OtelJMXMetricsCustomizerProvider implements AutoConfigurationCustomizerProvider {
-    private static final Duration DEFAULT_METRIC_EXPORT_INTERVAL = Duration.ofMinutes(1);
-    private static final Logger logger =
-            Logger.getLogger(OtelJMXMetricsCustomizerProvider.class.getName());
+  private static final Duration DEFAULT_METRIC_EXPORT_INTERVAL = Duration.ofMinutes(1);
+  private static final Logger logger =
+      Logger.getLogger(OtelJMXMetricsCustomizerProvider.class.getName());
 
-    private static final String OTEL_JMX_ENABLED_CONFIG = "otel.jmx.enabled";
-    private static final String OTEL_JMX_ENDPOINT_CONFIG = "otel.jmx.exporter.metrics.endpoint";
+  private static final String OTEL_JMX_ENABLED_CONFIG = "otel.jmx.enabled";
+  private static final String OTEL_JMX_ENDPOINT_CONFIG = "otel.jmx.exporter.metrics.endpoint";
 
-    public void customize(AutoConfigurationCustomizer autoConfiguration) {
-        autoConfiguration.addMeterProviderCustomizer(this::customizeMeterProvider);
+  public void customize(AutoConfigurationCustomizer autoConfiguration) {
+    autoConfiguration.addMeterProviderCustomizer(this::customizeMeterProvider);
+  }
+
+  private boolean isOtelJMXEnabled(ConfigProperties configProps) {
+    return configProps.getBoolean(OTEL_JMX_ENABLED_CONFIG)
+        && configProps.getString(OTEL_JMX_ENDPOINT_CONFIG, "") != "";
+  }
+
+  private SdkMeterProviderBuilder customizeMeterProvider(
+      SdkMeterProviderBuilder sdkMeterProviderBuilder, ConfigProperties configProps) {
+
+    if (isOtelJMXEnabled(configProps)) {
+      Set<String> registeredScopeNames = new HashSet<>(1);
+      String jmxRuntimeScopeName = "io.opentelemetry.jmx";
+      registeredScopeNames.add(jmxRuntimeScopeName);
+
+      configureMetricFilter(configProps, sdkMeterProviderBuilder, registeredScopeNames);
+
+      MetricExporter metricsExporter = JMXExporterProvider.INSTANCE.createExporter(configProps);
+      MetricReader metricReader =
+          ScopeBasedPeriodicMetricReader.create(metricsExporter, registeredScopeNames)
+              .setInterval(getMetricExportInterval(configProps))
+              .build();
+      sdkMeterProviderBuilder.registerMetricReader(metricReader);
+
+      logger.info("Otel JMX metric collection enabled");
+    }
+    return sdkMeterProviderBuilder;
+  }
+
+  private static void configureMetricFilter(
+      ConfigProperties configProps,
+      SdkMeterProviderBuilder sdkMeterProviderBuilder,
+      Set<String> registeredScopeNames) {
+    Set<String> exporterNames =
+        DefaultConfigProperties.getSet(configProps, "otel.metrics.exporter");
+    if (exporterNames.contains("none")) {
+      for (String scope : registeredScopeNames) {
+        sdkMeterProviderBuilder.registerView(
+            InstrumentSelector.builder().setMeterName(scope).build(),
+            View.builder().setAggregation(Aggregation.defaultAggregation()).build());
+
+        logger.log(Level.FINE, "Registered scope {0}", scope);
+      }
+      sdkMeterProviderBuilder.registerView(
+          InstrumentSelector.builder().setName("*").build(),
+          View.builder().setAggregation(Aggregation.drop()).build());
+    }
+  }
+
+  private static Duration getMetricExportInterval(ConfigProperties configProps) {
+    Duration exportInterval =
+        configProps.getDuration("otel.metric.export.interval", DEFAULT_METRIC_EXPORT_INTERVAL);
+    logger.log(Level.FINE, String.format("Otel JMX Metrics export interval: %s", exportInterval));
+    // Cap export interval to 60 seconds. This is currently required for metrics-trace correlation
+    // to work correctly.
+    if (exportInterval.compareTo(DEFAULT_METRIC_EXPORT_INTERVAL) > 0) {
+      exportInterval = DEFAULT_METRIC_EXPORT_INTERVAL;
+      logger.log(
+          Level.INFO,
+          String.format("Otel JMX Metrics export interval capped to %s", exportInterval));
+    }
+    return exportInterval;
+  }
+
+  private enum JMXExporterProvider {
+    INSTANCE;
+
+    public MetricExporter createExporter(ConfigProperties configProps) {
+      String protocol =
+          OtlpConfigUtil.getOtlpProtocol(OtlpConfigUtil.DATA_TYPE_METRICS, configProps);
+      logger.log(Level.FINE, String.format("Otel JMX metrics export protocol: %s", protocol));
+
+      String otelJMXEndpoint;
+      if (protocol.equals(OtlpConfigUtil.PROTOCOL_HTTP_PROTOBUF)) {
+        otelJMXEndpoint = configProps.getString(OTEL_JMX_ENDPOINT_CONFIG, "http://localhost:4314");
+        logger.log(
+            Level.FINE, String.format("Otel JMX metrics export endpoint: %s", otelJMXEndpoint));
+        return OtlpHttpMetricExporter.builder()
+            .setEndpoint(otelJMXEndpoint)
+            .setDefaultAggregationSelector(this::getAggregation)
+            .build();
+      }
+      throw new ConfigurationException("Unsupported Otel JMX metrics export protocol: " + protocol);
     }
 
-    private boolean isOtelJMXEnabled(ConfigProperties configProps) {
-        return configProps.getBoolean(OTEL_JMX_ENABLED_CONFIG);
+    private Aggregation getAggregation(InstrumentType instrumentType) {
+      if (instrumentType == InstrumentType.HISTOGRAM) {
+        return Aggregation.base2ExponentialBucketHistogram();
+      }
+      return Aggregation.defaultAggregation();
     }
-
-    private SdkMeterProviderBuilder customizeMeterProvider(
-            SdkMeterProviderBuilder sdkMeterProviderBuilder, ConfigProperties configProps) {
-
-        if (isOtelJMXEnabled(configProps)) {
-            Set<String> registeredScopeNames = new HashSet<>(1);
-            String jmxRuntimeScopeName = "io.opentelemetry.jmx";
-            registeredScopeNames.add(jmxRuntimeScopeName);
-
-            configureMetricFilter(configProps, sdkMeterProviderBuilder, registeredScopeNames);
-
-            MetricExporter metricsExporter = JMXExporterProvider.INSTANCE.createExporter(configProps);
-            MetricReader metricReader =
-                    ScopeBasedPeriodicMetricReader.create(metricsExporter, registeredScopeNames)
-                            .setInterval(getMetricExportInterval(configProps))
-                            .build();
-            sdkMeterProviderBuilder.registerMetricReader(metricReader);
-
-            logger.info("Otel JMX metric collection enabled");
-        }
-        return sdkMeterProviderBuilder;
-    }
-
-    private static void configureMetricFilter(
-            ConfigProperties configProps,
-            SdkMeterProviderBuilder sdkMeterProviderBuilder,
-            Set<String> registeredScopeNames) {
-        Set<String> exporterNames =
-                DefaultConfigProperties.getSet(configProps, "otel.metrics.exporter");
-        if (exporterNames.contains("none")) {
-            for (String scope : registeredScopeNames) {
-                sdkMeterProviderBuilder.registerView(
-                        InstrumentSelector.builder().setMeterName(scope).build(),
-                        View.builder().setAggregation(Aggregation.defaultAggregation()).build());
-
-                logger.log(Level.FINE, "Registered scope {0}", scope);
-            }
-            sdkMeterProviderBuilder.registerView(
-                    InstrumentSelector.builder().setName("*").build(),
-                    View.builder().setAggregation(Aggregation.drop()).build());
-        }
-    }
-
-    private static Duration getMetricExportInterval(ConfigProperties configProps) {
-        Duration exportInterval =
-                configProps.getDuration("otel.metric.export.interval", DEFAULT_METRIC_EXPORT_INTERVAL);
-        logger.log(Level.FINE, String.format("Otel JMX Metrics export interval: %s", exportInterval));
-        // Cap export interval to 60 seconds. This is currently required for metrics-trace correlation
-        // to work correctly.
-        if (exportInterval.compareTo(DEFAULT_METRIC_EXPORT_INTERVAL) > 0) {
-            exportInterval = DEFAULT_METRIC_EXPORT_INTERVAL;
-            logger.log(
-                    Level.INFO,
-                    String.format("Otel JMX Metrics export interval capped to %s", exportInterval));
-        }
-        return exportInterval;
-    }
-
-    private enum JMXExporterProvider {
-        INSTANCE;
-
-        public MetricExporter createExporter(ConfigProperties configProps) {
-            String protocol =
-                    OtlpConfigUtil.getOtlpProtocol(OtlpConfigUtil.DATA_TYPE_METRICS, configProps);
-            logger.log(Level.FINE, String.format("Otel JMX metrics export protocol: %s", protocol));
-
-            String otelJMXEndpoint;
-            if (protocol.equals(OtlpConfigUtil.PROTOCOL_HTTP_PROTOBUF)) {
-                otelJMXEndpoint = configProps.getString(OTEL_JMX_ENDPOINT_CONFIG, "http://localhost:4314");
-                logger.log(
-                        Level.FINE, String.format("Otel JMX metrics export endpoint: %s", otelJMXEndpoint));
-                return OtlpHttpMetricExporter.builder()
-                        .setEndpoint(otelJMXEndpoint)
-                        .setDefaultAggregationSelector(this::getAggregation)
-                        .setAggregationTemporalitySelector(CloudWatchTemporalitySelector.alwaysDelta())
-                        .build();
-            }
-            throw new ConfigurationException("Unsupported Otel JMX metrics export protocol: " + protocol);
-        }
-
-        private Aggregation getAggregation(InstrumentType instrumentType) {
-            if (instrumentType == InstrumentType.HISTOGRAM) {
-                return Aggregation.base2ExponentialBucketHistogram();
-            }
-            return Aggregation.defaultAggregation();
-        }
-    }
+  }
 }
