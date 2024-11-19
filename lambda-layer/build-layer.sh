87b785f06f9b0545e693619250521d551133f02a
Prashant Srivastava <srprash@amazon.com>
2024-11-19 09:18:27 -0800

    Revert "reverting to using adot1 after syncing with Michael He"
    This reverts commit dbc32a14cddee39ca0375607dd7b6f1945aa4c59.

--- lambda-layer/build-layer.sh
+++ lambda-layer/build-layer.sh
@@ -12,12 +12,7 @@ ADOT_VERSION="1.32.1"
 git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
 pushd opentelemetry-java-instrumentation
 git checkout v${OTEL_VERSION} -b tag-v${OTEL_VERSION}
-
-# This patch is for Lambda related context propagation
 patch -p1 < "$SOURCEDIR"/patches/opentelemetry-java-instrumentation.patch
-# There is another patch in the .github/patches directory for other changes. We should apply them too for consistency.
-patch -p1 < "$SOURCEDIR"/../.github/patches/opentelemetry-java-instrumentation.patch
-
 git add -A
 git commit -m "Create patch version"
 ./gradlew publishToMavenLocal
@@ -31,9 +26,22 @@ patch  -p1 < "${SOURCEDIR}"/patches/aws-otel-java-instrumentation.patch
 CI=false ./gradlew publishToMavenLocal -Prelease.version=${ADOT_VERSION}-adot-lambda1
 popd
 
-## Build ADOT Lambda Java SDK Layer Code
+# Build ADOT Lambda Java SDK Layer Code
 ./gradlew build
 
-## Copy ADOT Java Agent downloaded using Gradle task and bundle it with the Lambda handler script
+## Move the ADOT Lambda Java SDK code into OTel Lambda Java folder - TODO: probably not needed since this is related to wrapper
+#mkdir -p ../opentelemetry-lambda/java/layer-wrapper/build/extensions
+#cp ./build/libs/aws-otel-lambda-java-extensions.jar ../opentelemetry-lambda/java/layer-wrapper/build/extensions
+
+## Combine Java Agent build and ADOT Collector
+#pushd ./layer-javaagent/build/distributions || exit
+#unzip -qo opentelemetry-javaagent-layer.zip
+#rm opentelemetry-javaagent-layer.zip
+#mv otel-handler otel-handler-upstream
+#cp "$SOURCEDIR"/scripts/otel-handler .
+
+# Copy ADOT Java Agent downloaded using Gradle task
 cp "$SOURCEDIR"/build/javaagent/aws-opentelemetry-agent*.jar ./opentelemetry-javaagent.jar
+#unzip -qo ../../../../collector/build/opentelemetry-collector-layer-$1.zip - TODO: bundling collector not needed
 zip -qr opentelemetry-javaagent-layer.zip opentelemetry-javaagent.jar otel-handler
+#popd || exit
