bdf0d272d6f8bbdd1fea49ca453c5cf2e3fec1e3
Prashant Srivastava <srprash@amazon.com>
2024-11-27 14:56:40 -0800

    manage app through terraform

--- sample-apps/apigateway-lambda/src/main/java/com/amazon/sampleapp/LambdaHandler.java
+++ sample-apps/apigateway-lambda/src/main/java/com/amazon/sampleapp/LambdaHandler.java
@@ -2,13 +2,15 @@ package com.amazon.sampleapp;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.JSONObject;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 
-public class LambdaHandler implements RequestHandler<Object, String> {
+public class LambdaHandler implements RequestHandler<Object, Map<String, Object>> {
   private final OkHttpClient httpClient;
   private final S3Client s3Client;
 
@@ -18,15 +20,29 @@ public class LambdaHandler implements RequestHandler<Object, String> {
   }
 
   @Override
-  public String handleRequest(Object o, Context context) {
+  public Map<String, Object> handleRequest(Object o, Context context) {
     makeRemoteCall();
     listS3Buckets();
 
-    // Get the _X_AMZN_TRACE_ID environment variable
-    String traceId = System.getenv("_X_AMZN_TRACE_ID");
+    // Get the trace id from system property
+    // https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime
+    String traceId = System.getProperty("com.amazonaws.xray.traceHeader");
 
-    // Construct the response string
-    return "Trace ID: " + traceId;
+    // Construct the response body
+    JSONObject responseBody = new JSONObject();
+    responseBody.put("message", "Request successful");
+    responseBody.put("traceId", traceId);
+
+    // Return the API Gateway-compatible response
+    return Map.of(
+        "isBase64Encoded",
+        false,
+        "statusCode",
+        200,
+        "body",
+        responseBody.toString(),
+        "headers",
+        Map.of("Content-Type", "application/json"));
   }
 
   private void makeRemoteCall() {
