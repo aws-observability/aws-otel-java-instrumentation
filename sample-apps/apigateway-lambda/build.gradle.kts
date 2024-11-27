12ef8ce0d5aa9c2a7d7db1c025e54ac69cc02732
Prashant Srivastava <srprash@amazon.com>
2024-11-26 16:58:53 -0800

    apigateway-lambda sample app

---
+++ sample-apps/apigateway-lambda/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  java
+  application
+}
+
+application {
+  mainClass.set("com.amazon.sampleapp.LambdaHandler")
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
+}
+
+dependencies {
+  implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+  implementation("com.squareup.okhttp3:okhttp:4.11.0")
+  implementation("software.amazon.awssdk:s3:2.20.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.jar {
+  manifest {
+    attributes["Main-Class"] = "com.amazon.sampleapp.LambdaHandler"
+  }
+}
+
+tasks.register<Zip>("createLambdaZip") {
+  dependsOn("build")
+  from(tasks.compileJava.get())
+  from(tasks.processResources.get())
+  into("lib") {
+    from(configurations.runtimeClasspath.get())
+  }
+  archiveFileName.set("lambda-function.zip")
+  destinationDirectory.set(layout.buildDirectory.dir("distributions"))
+}
