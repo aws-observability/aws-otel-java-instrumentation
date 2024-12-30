5ea84596bb88992252c24b43870dec8cda270f38
Prashant Srivastava <50466688+srprash@users.noreply.github.com>
2024-12-30 11:00:16 -0800

    Updating Lambda Patch v1.33.6 + Fast Startup capability (#986)
    ### Description of changes:
    - Fast Startup feature was added as opt-in in the upstream some time
    ago: https://github.com/open-telemetry/opentelemetry-lambda/pull/1180
    (includes perf improvement results)
    - Did some of my own testing on a Java17 Lambda function with 3GB of
    memory and noticed cold start time improvement from 8.7 seconds to 4.2
    seconds.
    
    - Since the ADOT 1.x version has been updated to lated 1.33.6 version of
    upstream, we need to update the lambda layer related patches to this
    version as well.
    - Tested by manually building the layer and instrumenting a sample
    lambda function.
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- lambda-layer/build-layer.sh
+++ lambda-layer/build-layer.sh
@@ -4,6 +4,7 @@ SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 
 ## Get OTel version
+echo "Info: Getting OTEL Version"
 file="$SOURCEDIR/../.github/patches/versions"
 version=$(awk -F'=v' '/OTEL_JAVA_INSTRUMENTATION_VERSION/ {print $2}' "$file")
 echo "Found OTEL Version: ${version}"
@@ -15,15 +16,16 @@ fi
 
 
 ## Clone and Patch the OpenTelemetry Java Instrumentation Repository
+echo "Info: Cloning and Patching OpenTelemetry Java Instrumentation Repository"
 git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
 pushd opentelemetry-java-instrumentation
 git checkout v${version} -b tag-v${version}
 
-# This patch is for Lambda related context propagation
-patch -p1 < "$SOURCEDIR"/patches/opentelemetry-java-instrumentation.patch
-
 # There is another patch in the .github/patches directory for other changes. We should apply them too for consistency.
 patch -p1 < "$SOURCEDIR"/../.github/patches/opentelemetry-java-instrumentation.patch
+
+# This patch is for Lambda related context propagation
+patch -p1 < "$SOURCEDIR"/patches/opentelemetry-java-instrumentation.patch
 
 git add -A
 git commit -m "Create patch version"
@@ -33,6 +35,7 @@ rm -rf opentelemetry-java-instrumentation
 
 
 ## Build the ADOT Java from current source
+echo "Info: Building ADOT Java from current source"
 pushd "$SOURCEDIR"/..
 patch  -p1 < "${SOURCEDIR}"/patches/aws-otel-java-instrumentation.patch
 CI=false ./gradlew publishToMavenLocal -Prelease.version=${version}-adot-lambda1
@@ -40,9 +43,11 @@ popd
 
 
 ## Build ADOT Lambda Java SDK Layer Code
-./gradlew build
+echo "Info: Building ADOT Lambda Java SDK Layer Code"
+./gradlew build -PotelVersion=${version}
 
 
 ## Copy ADOT Java Agent downloaded using Gradle task and bundle it with the Lambda handler script
+echo "Info: Creating the layer artifact"
 cp "$SOURCEDIR"/build/javaagent/aws-opentelemetry-agent*.jar ./opentelemetry-javaagent.jar
 zip -qr opentelemetry-javaagent-layer.zip opentelemetry-javaagent.jar otel-instrument
