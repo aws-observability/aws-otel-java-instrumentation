3dbbae042dca6e77b7e33976d8ff3ef04d0333db
Jeel Mehta <jeelmm@amazon.com>
2025-03-04 14:15:22 -0800

    Fixing formatting issues

--- exporters/aws-otel-otlp-udp-exporter/build.gradle.kts
+++ exporters/aws-otel-otlp-udp-exporter/build.gradle.kts
@@ -1,99 +1,114 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 plugins {
-    id("java")
-    id("java-library")
-    id("maven-publish")
+  id("java")
+  id("java-library")
+  id("maven-publish")
 }
 
 group = "software.opentelemetry.exporters.otlp.udp"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenLocal()
-    mavenCentral()
+  mavenLocal()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(platform("io.opentelemetry:opentelemetry-bom:1.44.1"))
-    implementation("io.opentelemetry:opentelemetry-api")
-    implementation("io.opentelemetry:opentelemetry-sdk")
-    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
-    implementation("io.opentelemetry:opentelemetry-exporter-otlp-common")
-    implementation("io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha")
-    implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("org.apache.logging.log4j:log4j-api:2.24.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.24.1")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.mockito:mockito-core:5.3.1")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
+  implementation(platform("io.opentelemetry:opentelemetry-bom:1.44.1"))
+  implementation("io.opentelemetry:opentelemetry-api")
+  implementation("io.opentelemetry:opentelemetry-sdk")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp-common")
+  implementation("io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha")
+  implementation("com.google.code.findbugs:jsr305:3.0.2")
+  implementation("org.apache.logging.log4j:log4j-api:2.24.1")
+  implementation("org.apache.logging.log4j:log4j-core:2.24.1")
+  implementation("org.slf4j:slf4j-simple:2.0.16")
+  testImplementation(platform("org.junit:junit-bom:5.9.2"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation("org.mockito:mockito-core:5.3.1")
+  testImplementation("org.assertj:assertj-core:3.24.2")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
 }
 
 java {
-    withSourcesJar()
-    withJavadocJar()
+  withSourcesJar()
+  withJavadocJar()
 }
 
 tasks.javadoc {
-    options {
-        (this as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
-    }
-    isFailOnError = false
+  options {
+    (this as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
+  }
+  isFailOnError = false
 }
 
 sourceSets {
-    main {
-        java {
-            srcDirs("src/main/java")
-        }
-        resources {
-            srcDirs("src/main/resources")
-        }
+  main {
+    java {
+      srcDirs("src/main/java")
     }
-    test {
-        java {
-            srcDirs("src/test/java")
-        }
-        resources {
-            srcDirs("src/test/resources")
-        }
+    resources {
+      srcDirs("src/main/resources")
     }
+  }
+  test {
+    java {
+      srcDirs("src/test/java")
+    }
+    resources {
+      srcDirs("src/test/resources")
+    }
+  }
 }
 
 tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
 }
 
 tasks.jar {
-    manifest {
-        attributes(
-            "Implementation-Title" to project.name,
-            "Implementation-Version" to project.version
-        )
-    }
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  manifest {
+    attributes(
+      "Implementation-Title" to project.name,
+      "Implementation-Version" to project.version,
+    )
+  }
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 tasks.named<Jar>("javadocJar") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 tasks.named<Jar>("sourcesJar") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = project.group.toString()
-            artifactId = "aws-otel-otlp-udp-exporter"
-            version = project.version.toString()
-        }
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+      groupId = project.group.toString()
+      artifactId = "aws-otel-otlp-udp-exporter"
+      version = project.version.toString()
     }
+  }
 }
