37bf04095d68b442818e005c304c2ffb011b1d9f
Jeel Mehta <jeelmm@amazon.com>
2025-03-07 15:07:32 -0800

    Adding the release workflow of UDP exporter and also the sample application for validation

---
+++ sample-apps/integ-test-app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("java")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("software.opentelemetry.exporters.otlp.udp:aws-otel-otlp-udp-exporter:0.0.1")
+    implementation("io.opentelemetry:opentelemetry-api")
+    implementation("io.opentelemetry:opentelemetry-sdk")
+}
+
+tasks.bootJar {
+    archiveFileName.set("integ-test-app.jar")
+    mainClass.set("com.amazon.sampleapp.ValidationApp")
+}
