fcbce919e6cbebbc339e4c5594d713e34e1a15e9
Thomas Pierce <thp@amazon.com>
2025-05-28 13:32:15 -0700

    Update otelSnapshotVersion to 2.12.0

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -28,7 +28,7 @@ val testSnapshots = rootProject.findProperty("testUpstreamSnapshots") == "true"
 
 // This is the version of the upstream instrumentation BOM
 val otelVersion = "2.11.0-adot1"
-val otelSnapshotVersion = "2.11.0"
+val otelSnapshotVersion = "2.12.0"
 val otelAlphaVersion = if (!testSnapshots) "$otelVersion-alpha" else "$otelSnapshotVersion-alpha-SNAPSHOT"
 val otelJavaAgentVersion = if (!testSnapshots) otelVersion else "$otelSnapshotVersion-SNAPSHOT"
 // All versions below are only used in testing and do not affect the released artifact.
