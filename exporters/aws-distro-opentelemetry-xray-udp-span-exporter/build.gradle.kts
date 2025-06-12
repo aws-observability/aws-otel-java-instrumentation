2f880b8659db9d94d70741778e26a7ea36ad690b
Steve Liu <liustve@amazon.com>
2025-06-12 14:06:58 -0700

    test

--- exporters/aws-distro-opentelemetry-xray-udp-span-exporter/build.gradle.kts
+++ exporters/aws-distro-opentelemetry-xray-udp-span-exporter/build.gradle.kts
@@ -101,10 +101,10 @@ tasks.create("printVersion") {
 nexusPublishing {
   repositories {
     sonatype {
-      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-      username.set(System.getenv("PUBLISH_TOKEN_USERNAME_TEST"))
-      password.set(System.getenv("PUBLISH_TOKEN_PASSWORD_TEST"))
+      nexusUrl.set(uri("https://aws.oss.sonatype.org/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://aws.oss.sonatype.org/content/repositories/snapshots/"))
+      username.set(System.getenv("PUBLISH_TOKEN_USERNAME"))
+      password.set(System.getenv("PUBLISH_TOKEN_PASSWORD"))
     }
   }
 }
