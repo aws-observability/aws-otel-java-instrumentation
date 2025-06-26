806a9db6f87e72374b2bd611a15b88e37b7315a1
Anahat Nagra <anahat@amazon.com>
2025-06-26 13:28:29 -0700

    created aws-sdk package for v2.2 and added files patched in upstream and their necessary dependencies

---
+++ instrumentation/aws-sdk/src/main/java/software/amazon/opentelemetry/javaagent/instrumentation/awssdk_v2_2/FieldMapper.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.opentelemetry.javaagent.instrumentation.awssdk_v2_2;
+
+import io.opentelemetry.api.trace.Span;
+import java.util.List;
+import java.util.function.Function;
+import javax.annotation.Nullable;
+import software.amazon.awssdk.core.SdkRequest;
+import software.amazon.awssdk.core.SdkResponse;
+import software.amazon.awssdk.utils.StringUtils;
+
+/*
+ * This class contains both patching logic and code copied from OTel aws-sdk-2.2.
+ */
+class FieldMapper {
+
+  private final Serializer serializer;
+  private final MethodHandleFactory methodHandleFactory;
+
+  FieldMapper() {
+    serializer = new Serializer();
+    methodHandleFactory = new MethodHandleFactory();
+  }
+
+  FieldMapper(Serializer serializer, MethodHandleFactory methodHandleFactory) {
+    this.methodHandleFactory = methodHandleFactory;
+    this.serializer = serializer;
+  }
+
+  void mapToAttributes(SdkRequest sdkRequest, AwsSdkRequest request, Span span) {
+    mapToAttributes(
+        field -> sdkRequest.getValueForField(field, Object.class).orElse(null),
+        FieldMapping.Type.REQUEST,
+        request,
+        span);
+  }
+
+  void mapToAttributes(SdkResponse sdkResponse, AwsSdkRequest request, Span span) {
+    mapToAttributes(
+        field -> sdkResponse.getValueForField(field, Object.class).orElse(null),
+        FieldMapping.Type.RESPONSE,
+        request,
+        span);
+  }
+
+  private void mapToAttributes(
+      Function<String, Object> fieldValueProvider,
+      FieldMapping.Type type,
+      AwsSdkRequest request,
+      Span span) {
+    for (FieldMapping fieldMapping : request.fields(type)) {
+      mapToAttributes(fieldValueProvider, fieldMapping, span);
+    }
+    for (FieldMapping fieldMapping : request.type().fields(type)) {
+      mapToAttributes(fieldValueProvider, fieldMapping, span);
+    }
+  }
+
+  // Contains patching logic
+  private void mapToAttributes(
+      Function<String, Object> fieldValueProvider, FieldMapping fieldMapping, Span span) {
+    // traverse path
+    List<String> path = fieldMapping.getFields();
+    Object target = fieldValueProvider.apply(path.get(0));
+    for (int i = 1; i < path.size() && target != null; i++) {
+      target = next(target, path.get(i));
+    }
+    String value;
+    if (target != null) {
+      if (AwsExperimentalAttributes.isGenAiAttribute(fieldMapping.getAttribute())) {
+        value = serializer.serialize(fieldMapping.getAttribute(), target);
+      } else {
+        value = serializer.serialize(target);
+      }
+      if (!StringUtils.isEmpty(value)) {
+        span.setAttribute(fieldMapping.getAttribute(), value);
+      }
+    }
+  }
+
+  @Nullable
+  private Object next(Object current, String fieldName) {
+    try {
+      return methodHandleFactory.forField(current.getClass(), fieldName).invoke(current);
+    } catch (Throwable t) {
+      // ignore
+    }
+    return null;
+  }
+}
---
+++ instrumentation/aws-sdk/src/main/java/software/amazon/opentelemetry/javaagent/instrumentation/awssdk_v2_2/MethodHandleFactory.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.opentelemetry.javaagent.instrumentation.awssdk_v2_2;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*
+ * This class is fully copied from OTel's aws-sdk-2.2.
+ */
+class MethodHandleFactory {
+
+  private static String unCapitalize(String string) {
+    return string.substring(0, 1).toLowerCase(Locale.ROOT) + string.substring(1);
+  }
+
+  private final ClassValue<ConcurrentHashMap<String, MethodHandle>> getterCache =
+      new ClassValue<ConcurrentHashMap<String, MethodHandle>>() {
+        @Override
+        protected ConcurrentHashMap<String, MethodHandle> computeValue(Class<?> type) {
+          return new ConcurrentHashMap<>();
+        }
+      };
+
+  MethodHandle forField(Class<?> clazz, String fieldName)
+      throws NoSuchMethodException, IllegalAccessException {
+    MethodHandle methodHandle = getterCache.get(clazz).get(fieldName);
+    if (methodHandle == null) {
+      // getter in AWS SDK is lowercased field name
+      methodHandle =
+          MethodHandles.publicLookup().unreflect(clazz.getMethod(unCapitalize(fieldName)));
+      getterCache.get(clazz).put(fieldName, methodHandle);
+    }
+    return methodHandle;
+  }
+}
