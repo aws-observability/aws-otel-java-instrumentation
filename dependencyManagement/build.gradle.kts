20dec6409f356510bc123552024737f4f7d76f57
Jeel Mehta <jeelmm@amazon.com>
2025-08-07 11:07:56 -0700

    Update dependencies and Otel to v2.12.0

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -27,36 +27,36 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val testSnapshots = rootProject.findProperty("testUpstreamSnapshots") == "true"
 
 // This is the version of the upstream instrumentation BOM
-val otelVersion = "2.11.0-adot1"
-val otelSnapshotVersion = "2.12.0"
+val otelVersion = "2.12.0"
+val otelSnapshotVersion = "2.13.0"
 val otelAlphaVersion = if (!testSnapshots) "$otelVersion-alpha" else "$otelSnapshotVersion-alpha-SNAPSHOT"
 val otelJavaAgentVersion = if (!testSnapshots) otelVersion else "$otelSnapshotVersion-SNAPSHOT"
 // All versions below are only used in testing and do not affect the released artifact.
 
 val dependencyBoms = listOf(
-  "com.amazonaws:aws-java-sdk-bom:1.12.599",
-  "com.fasterxml.jackson:jackson-bom:2.16.0",
-  "com.google.guava:guava-bom:33.0.0-jre",
+  "com.amazonaws:aws-java-sdk-bom:1.12.788",
+  "com.fasterxml.jackson:jackson-bom:2.19.2",
+  "com.google.guava:guava-bom:33.4.8-jre",
   "com.google.protobuf:protobuf-bom:3.25.1",
-  "com.linecorp.armeria:armeria-bom:1.26.4",
-  "io.grpc:grpc-bom:1.59.1",
+  "com.linecorp.armeria:armeria-bom:1.33.0",
+  "io.grpc:grpc-bom:1.74.0",
   "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:$otelAlphaVersion",
-  "org.apache.logging.log4j:log4j-bom:2.21.1",
-  "org.junit:junit-bom:5.10.1",
+  "org.apache.logging.log4j:log4j-bom:2.25.1",
+  "org.junit:junit-bom:5.13.4",
   "org.springframework.boot:spring-boot-dependencies:2.7.17",
-  "org.testcontainers:testcontainers-bom:1.19.3",
-  "software.amazon.awssdk:bom:2.30.17",
+  "org.testcontainers:testcontainers-bom:1.21.3",
+  "software.amazon.awssdk:bom:2.32.16",
 )
 
 val dependencySets = listOf(
   DependencySet(
     "org.assertj",
-    "3.24.2",
+    "3.27.3",
     listOf("assertj-core"),
   ),
   DependencySet(
     "org.curioswitch.curiostack",
-    "2.2.0",
+    "2.8.1",
     listOf("protobuf-jackson"),
   ),
   DependencySet(
@@ -70,15 +70,15 @@ val dependencySets = listOf(
 )
 
 val dependencyLists = listOf(
-  "commons-logging:commons-logging:1.2",
+  "commons-logging:commons-logging:1.3.5",
   "com.sparkjava:spark-core:2.9.4",
   "com.squareup.okhttp3:okhttp:4.12.0",
-  "io.opentelemetry.contrib:opentelemetry-aws-xray:1.39.0-adot1",
-  "io.opentelemetry.contrib:opentelemetry-aws-resources:1.39.0-alpha",
-  "io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha",
+  "io.opentelemetry.contrib:opentelemetry-aws-xray:1.48.0",
+  "io.opentelemetry.contrib:opentelemetry-aws-resources:1.48.0-alpha",
+  "io.opentelemetry.proto:opentelemetry-proto:1.7.0-alpha",
   "io.opentelemetry.javaagent:opentelemetry-javaagent:$otelJavaAgentVersion",
   "io.opentelemetry:opentelemetry-extension-aws:1.20.1",
-  "net.bytebuddy:byte-buddy:1.14.10",
+  "net.bytebuddy:byte-buddy:1.17.6",
 )
 
 javaPlatform {
